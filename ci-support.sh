0ea0be257b3b5fe504ac6799f7324edcbf768e2d
darkwizard242 <am900820@gmail.com>
2020-02-15 00:51:15 -0500

    echo vars and corrections

--- ci-support.sh
+++ ci-support.sh
@@ -1,5 +1,5 @@
-#!/bin/bash
-set -ev
+#!/bin/bash -x
+set -e
 
 BUILDSCRIPT_MASTER="build.json"
 BUILDSCRIPT_NON_MASTER="template-branch-master.json"
@@ -10,9 +10,11 @@ then
   echo -e "\nBranch is: ${TRAVIS_BRANCH}"
 else
   echo -e "\nBranch is: ${TRAVIS_BRANCH}"
+  echo -e "\nPath is: ${PATH_TO_BUILDSCRIPT}"
+  echo -e "\nPacker template file is: ${BUILDSCRIPT_NON_MASTER}"
   cd ${PATH_TO_BUILDSCRIPT}
-  echo -e "\nValidating packer template file: {BUILDSCRIPT_NON_MASTER}"
+  echo -e "\nValidating packer template file: ${BUILDSCRIPT_NON_MASTER}"
   ../$BUILDER validate ${BUILDSCRIPT_NON_MASTER}
-  echo -e "\nVRunning packer build for template file: {BUILDSCRIPT_NON_MASTER}"
+  echo -e "\nRunning packer build for template file: ${BUILDSCRIPT_NON_MASTER}"
   sudo ../$BUILDER build -timestamp-ui -color=false ${BUILDSCRIPT_NON_MASTER}
 fi
