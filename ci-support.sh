6608cae0083cc280c450a886d05106f965de3016
darkwizard242 <am900820@gmail.com>
2020-02-15 00:40:58 -0500

    ci support logic updated for complex solutions

--- ci-support.sh
+++ ci-support.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
 set -ev
 
+BUILDSCRIPT_MASTER="build.json"
+BUILDSCRIPT_NON_MASTER="template-branch-master.json"
+
 
 if [ "${TRAVIS_BRANCH}" = "master" ];
 then
-  echo -e "\nBranch is: master"
+  echo -e "\nBranch is: ${TRAVIS_BRANCH}"
 else
-  echo -e "\nBranch is not: master"
+  echo -e "\nBranch is: ${TRAVIS_BRANCH}"
+  cd ${PATH_TO_BUILDSCRIPT}
+  echo -e "\nValidating packer template file: {BUILDSCRIPT_NON_MASTER}"
+  ../$BUILDER validate ${BUILDSCRIPT_NON_MASTER}
+  echo -e "\nVRunning packer build for template file: {BUILDSCRIPT_NON_MASTER}"
+  sudo ../$BUILDER build -timestamp-ui -color=false ${BUILDSCRIPT_NON_MASTER}
 fi
