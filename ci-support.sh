071410f177f73be95d2ad758f81a4ad4010a94ac
darkwizard242 <am900820@gmail.com>
2020-02-15 01:00:07 -0500

    remove debugging steps and set tabbing

--- ci-support.sh
+++ ci-support.sh
@@ -1,5 +1,4 @@
-#!/bin/bash -x
-set -e
+#!/bin/bash -e
 
 BUILDSCRIPT_MASTER="build.json"
 BUILDSCRIPT_NON_MASTER="template-branch-others.json"
@@ -7,15 +6,14 @@ BUILDSCRIPT_NON_MASTER="template-branch-others.json"
 
 if [ "${TRAVIS_BRANCH}" = "master" ];
 then
-  echo -e "\nBranch is: ${TRAVIS_BRANCH}"
+  echo -e "\nBranch is:\t${TRAVIS_BRANCH}"
 else
-  echo -e "\nBranch is: ${TRAVIS_BRANCH}"
-  echo -e "\nPath is: ${PATH_TO_BUILDSCRIPT}"
-  echo -e "\nPacker template file is: ${BUILDSCRIPT_NON_MASTER}"
+  echo -e "\nBranch is:\t${TRAVIS_BRANCH}"
+  echo -e "\nPath is:\t${PATH_TO_BUILDSCRIPT}"
+  echo -e "\nPacker template file is:\t${BUILDSCRIPT_NON_MASTER}"
   cd ${PATH_TO_BUILDSCRIPT}
-  pwd
-  echo -e "\nValidating packer template file: ${BUILDSCRIPT_NON_MASTER}"
+  echo -e "\nValidating packer template file:\t${BUILDSCRIPT_NON_MASTER}"
   ../$BUILDER validate ${BUILDSCRIPT_NON_MASTER}
-  echo -e "\nRunning packer build for template file: ${BUILDSCRIPT_NON_MASTER}"
+  echo -e "\nRunning packer build for template file:\t${BUILDSCRIPT_NON_MASTER}"
   sudo ../$BUILDER build -timestamp-ui -color=false ${BUILDSCRIPT_NON_MASTER}
 fi
