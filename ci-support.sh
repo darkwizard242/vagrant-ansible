7d431c02e8833fb61be645e0867d61aca2b8b36f
darkwizard242 <am900820@gmail.com>
2020-02-14 23:49:33 -0500

    pass arg to echo

--- ci-support.sh
+++ ci-support.sh
@@ -4,7 +4,7 @@ set -ev
 
 if [ "${TRAVIS_BRANCH}" = "master" ];
 then
-  echo "\nBranch is: master"
+  echo -e "\nBranch is: master"
 else
-  echo "\nBranch is not: master"
+  echo -e "\nBranch is not: master"
 fi
