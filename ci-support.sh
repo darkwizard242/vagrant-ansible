c7b54ec05854e4acee8fa749c4334c36887b42d2
darkwizard242 <am900820@gmail.com>
2020-02-19 15:49:09 -0500

    unset export as not required

--- ci-support.sh
+++ ci-support.sh
@@ -12,12 +12,11 @@ build_template () {
   echo -e "\nValidating packer template file: $1"
   ../"${BUILDER}" validate "$1"
   echo -e "\nRunning packer build for template file: $1"
-  sudo ../"${BUILDER}" build -timestamp-ui -color=false -force -var 'box=${BOX}' "$1"
+  sudo PACKER_LOG=1 ../"${BUILDER}" build -timestamp-ui -color=false -force -var 'box=${BOX}' "$1"
 }
 
 if [[ "${TRAVIS_BRANCH}" = "master" && ${TRAVIS_PULL_REQUEST_SLUG} = "" ]];
 then
-  export PACKER_LOG=1
   build_template ${BUILDSCRIPT_MASTER}
 elif [ "${TRAVIS_PULL_REQUEST_BRANCH}" = "master" ];
 then
